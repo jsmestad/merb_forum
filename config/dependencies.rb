c38684a185d70d180135c4b2e2f819d8bd1b5aa6
Justin Smestad <justin.smestad@gmail.com>
2009-01-23 22:51:21 -0700

    new dependency set

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,6 +1,6 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.6.1"
-dm_gems_version   = "0.9.8"
+merb_gems_version = "1.0.8.1"
+dm_gems_version   = "0.9.10"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-action-args", merb_gems_version
@@ -24,5 +24,8 @@ dependency "dm-types", dm_gems_version
 dependency "dm-validations", dm_gems_version  
 
 # Merbherd Dependencies
+dependency "genki-dm-pagination", "0.1.2", :require_as => 'dm-pagination'
+dependency "teamon-merb-colorful-logger", "0.1.1", :require_as => 'merb-colorful-logger'
+dependency "thoughtbot-factory_girl", "1.1.5", :require_as => 'factory_girl'
 dependency "cpjolicoeur-bb-ruby", "~> 0.8.1", :require_as => 'bb-ruby'
 dependency "coderay"
